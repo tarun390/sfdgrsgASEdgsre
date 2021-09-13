5c1490b6387787ad47db8295f5f52f3e27305139
tarun390 <73883651+tarun390@users.noreply.github.com>
2021-09-13 19:19:44 +0530

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import csv
+START_URL = "https://en.wikipedia.org/wiki/List_of_brightest_stars_and_other_record_stars"
+browser = webdriver.Chrome("/Users/Admin/Downloads/chromedriver")
+browser.get(START_URL)
+time.sleep(10)
+def scrape():
+    headers = ["name", "distance", "mass", "radius"]
+    planet_data = []
+    for i in range(0, 428):
+        soup = BeautifulSoup(browser.page_source, "html.parser")
+        for ul_tag in soup.find_all("th", attrs={"class", "headerSort"}):
+            li_tags = ul_tag.find_all("tr")
+            temp_list = []
+            for index, li_tag in enumerate(li_tags):
+                if index == 0:
+                    temp_list.append(li_tag.find_all("a")[0].contents[0])
+                else:
+                    try:
+                        temp_list.append(li_tag.contents[0])
+                    except:
+                        temp_list.append("")
+            planet_data.append(temp_list)
+        browser.find_element_by_xpath('//*[@id="mw-content-text"]/div[1]/table').click()
+    with open("scrapper_2.csv", "w") as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(headers)
+        csvwriter.writerows(planet_data)
+scrape()
